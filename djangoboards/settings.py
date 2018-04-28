b8e4db851a158fe4b24544f7eea6e899176d019f
Annihilater <annihilater@icloud.com>
2018-04-28 09:39:42 +0800

    Set DEBUG = False

--- djangoboards/settings.py
+++ djangoboards/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w_8*^^5-usw=j6ob78ae1noz-!^b#ts+x6jvj#y01ti%usew=j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
