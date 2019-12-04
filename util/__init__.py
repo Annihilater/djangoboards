abbd4f1e60860c3a371452ec44d46cc5648559c5
Annihilater <yanmiexingkong@gmail.com>
2019-12-04 17:27:10 +0800

    chore: update @Author and @email

--- util/__init__.py
+++ util/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Date  : 2019-04-14 12:39
-# @Author: PythonVampire
-# @email : vampire@ivamp.cn
+# @Author: yanmiexingkong
+# @email : yanmiexingkong@gmail.com
 # @File  : __init__.py.py
 
 
