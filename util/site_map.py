410e8207cb42f3cecba218ada16c113cca0e71e2
Annihilater <yanmiexingkong@gmail.com>
2019-12-09 19:52:44 +0800

    feat: add sitemap view function

--- util/site_map.py
+++ util/site_map.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Date  : 2019-04-14 12:40
-# @Author: PythonVampire
-# @email : vampire@ivamp.cn
+# @Author: yanmiexingkong
+# @email : yanmiexingkong@gmail.com
 # @File  : site_map.py
+from django.contrib import sitemaps
+from django.urls import reverse
 
 
+class StaticViewSitemap(sitemaps.Sitemap):
+    priority = 1.0
+    changefreq = 'daily'
+
+    def items(self):
+        return ['home']
+
+    def location(self, item):
+        return reverse(item)
+
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
